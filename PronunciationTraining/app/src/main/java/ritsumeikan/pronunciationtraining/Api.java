e266c5f3353986b1d1e29f3ac5aa208fa0a07433
phanlacphuc <phanlacphuc@gmail.com>
2016-01-04 21:39:25 +0900

    login with facebook

--- PronunciationTraining/app/src/main/java/ritsumeikan/pronunciationtraining/Api.java
+++ PronunciationTraining/app/src/main/java/ritsumeikan/pronunciationtraining/Api.java
@@ -1,9 +1,10 @@
 package ritsumeikan.pronunciationtraining;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
@@ -14,6 +15,7 @@ import cz.msebera.android.httpclient.Header;
  * Created by phanlacphuc on 12/28/15.
  */
 public class Api {
+    private static String TAG = "Api";
     private static Api ourInstance = new Api();
     private static String BASE_URL = "http://130.211.164.255:3000/";
 
@@ -26,112 +28,60 @@ public class Api {
     private Api() {
     }
 
-    public void login(Context context, String facebookId, String name, String avatar) {
+    public interface OnCustomSuccessListener{
+        public void onSuccess();   //method, which can have parameters
+    }
+
+    public void login(Context context, String facebookId, String name, String avatar, final OnCustomSuccessListener onCustomSuccessListener) {
         RequestParams params = new RequestParams();
         params.add("facebookId", facebookId);
         params.add("name", name);
         params.add("avatar", avatar);
-        mClient.post(BASE_URL + "login", params, new AsyncHttpResponseHandler() {
-
+        mClient.post(BASE_URL + "login", params, new JsonHttpResponseHandler() {
             @Override
-            public void onStart() {
-                // called before request is started
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 // called when response HTTP status is "200 OK"
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-            }
-
-            @Override
-            public void onRetry(int retryNo) {
-                // called when request is retried
+                Log.d(TAG, "response = " + response);
+                onCustomSuccessListener.onSuccess();
             }
         });
     }
-    public void createClass(Context context, JSONArray wordArray, String facebookId) {
+    public void createClass(JSONArray wordArray, String facebookId, final OnCustomSuccessListener onCustomSuccessListener) {
         RequestParams params = new RequestParams();
         params.add("wordArray", wordArray.toString());
         params.add("facebookId", facebookId);
-        mClient.post(BASE_URL + "createClass", params, new AsyncHttpResponseHandler() {
-
+        mClient.post(BASE_URL + "createClass", params, new JsonHttpResponseHandler() {
             @Override
-            public void onStart() {
-                // called before request is started
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 // called when response HTTP status is "200 OK"
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-            }
-
-            @Override
-            public void onRetry(int retryNo) {
-                // called when request is retried
+                Log.d(TAG, "response = " + response);
+                onCustomSuccessListener.onSuccess();
             }
         });
     }
-    public void inviteFriend(Context context, String classId, String email) {
+    public void inviteFriend(String classId, String email, final OnCustomSuccessListener onCustomSuccessListener) {
         RequestParams params = new RequestParams();
         params.add("classId", classId);
         params.add("email", email);
-        mClient.post(BASE_URL + "inviteFriend", params, new AsyncHttpResponseHandler() {
-
+        mClient.post(BASE_URL + "inviteFriend", params, new JsonHttpResponseHandler() {
             @Override
-            public void onStart() {
-                // called before request is started
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 // called when response HTTP status is "200 OK"
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-            }
-
-            @Override
-            public void onRetry(int retryNo) {
-                // called when request is retried
+                Log.d(TAG, "response = " + response);
+                onCustomSuccessListener.onSuccess();
             }
         });
     }
-    public void joinClass(Context context, String facebookId, String inviteCode) {
+    public void joinClass(String facebookId, String inviteCode, final OnCustomSuccessListener onCustomSuccessListener) {
         RequestParams params = new RequestParams();
         params.add("facebookId", facebookId);
         params.add("inviteCode", inviteCode);
-        mClient.post(BASE_URL + "joinClass", params, new AsyncHttpResponseHandler() {
-
+        mClient.post(BASE_URL + "joinClass", params, new JsonHttpResponseHandler() {
             @Override
-            public void onStart() {
-                // called before request is started
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 // called when response HTTP status is "200 OK"
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-            }
-
-            @Override
-            public void onRetry(int retryNo) {
-                // called when request is retried
+                Log.d(TAG, "response = " + response);
+                onCustomSuccessListener.onSuccess();
             }
         });
     }
--- PronunciationTraining/app/src/main/java/ritsumeikan/pronunciationtraining/TopScreenActivity.java
+++ PronunciationTraining/app/src/main/java/ritsumeikan/pronunciationtraining/TopScreenActivity.java
@@ -1,16 +1,80 @@
 package ritsumeikan.pronunciationtraining;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class TopScreenActivity extends ActionBarActivity {
+
+    static String TAG = "TopScreenActivity";
+    private CallbackManager mCallbackManager;
+    private ProfileTracker mProfileTracker;
+    private Profile mProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // Initialize the SDK before executing any other operations,
+        // especially, if you're using Facebook UI elements.
+
         setContentView(R.layout.activity_top_screen);
+
+        final LoginButton loginButton = (LoginButton) this.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile");
+        // If using in a fragment
+        //loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        mCallbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.d(TAG, "facebook login successed. loginResult: " + loginResult.toString());
+
+                mProfile = Profile.getCurrentProfile();
+                if (mProfile != null) {
+                    login();
+                } else {
+                    mProfileTracker = new ProfileTracker() {
+                        @Override
+                        protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
+                            mProfile = profile2;
+                            mProfileTracker.stopTracking();
+                            login();
+                        }
+                    };
+                    mProfileTracker.startTracking();
+                }
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.d(TAG, "facebook login cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                exception.printStackTrace();
+            }
+        });
     }
 
     @Override
@@ -33,5 +97,39 @@ public class TopScreenActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void login() {
+        if (mProfile != null) {
+            Api.getInstance().login(this, mProfile.getId(), mProfile.getName(), mProfile.getProfilePictureUri(100, 100).toString(), new Api.OnCustomSuccessListener() {
+                @Override
+                public void onSuccess() {
+                    // TODO: after login
+                    Log.d(TAG, "login success");
+                }
+            });
+        }
     }
 }
